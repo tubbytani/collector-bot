0e2cc01de8189d233c02affe151f032edfd94720
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-01-02 06:49:09 +0530

    adc

---
+++ eyrc2.c
@@ -0,0 +1,110 @@
+/*
+ * Eyantracb.c
+ *
+ * Created: 12/30/2019 3:50:18 PM
+ * Author : new
+ */ 
+#define  F_CPU 14745600
+#include <avr/io.h>
+#include<util/delay.h>
+
+
+void adc_init()
+{
+	ADMUX=(1<<REFS0)|(1<<ADLAR);
+	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // prescaler = 128 did not enable the interrupt feature
+}
+void adc_ans()
+{   
+	adc_init();
+	ADCSRA |= (1<<ADSC);//start conversion
+	while(ADCSRA&(1<<ADSC));
+	return(ADC);
+}
+void motor_init()
+{
+	
+DDRA=0x0F;
+PORTA=0x00;
+	
+}
+void forward()
+{   
+	motor_init();
+	PORTA=0x05;
+}
+void backward()
+{
+	
+	motor_init();
+	PORTA=0x0A;
+}
+void right_turn()
+{
+	motor_init();
+	PORTA=0x06;
+}
+void left_turn()
+{
+motor_init();
+PORTA=0x09;	
+}
+void buzzer()
+{
+	DDRB=0x01;
+	PORTB=0x01;
+	_delay_ms(2000);
+	PORTB=0x00;
+}
+void stop()
+{
+	motor_init();
+}
+void velocity_init()
+{
+	DDRL=0x30;//4 and 5 pin
+	PORTL=0x30;//pull down types
+		
+}
+	
+void velocity(const int speed1,const int speed2)
+{
+	
+velocity_init();
+OCR0A=speed1;//using 8bit timers
+OCR0B=speed2;
+
+}	
+void soft_left()
+{
+	motor_init();
+	PORTA=0x02;	
+	
+}
+void soft_right()
+{
+	motor_init();
+	PORTA=0x04;
+}
+int main(void)
+{
+	/* Replace with your application code */
+
+	while (1)
+	{
+		forward();
+		_delay_ms(1500);//as diameter is 5cm thus to cover 10cm it would require 1.5seconds
+		left_turn();
+		_delay_ms(500);
+		forward();
+		_delay_ms(1500);
+		right_turn();
+		_delay_ms(500);
+		forward();
+		_delay_ms(1500);
+		backward();
+		stop();
+		buzzer();
+		stop();
+	}
+}
