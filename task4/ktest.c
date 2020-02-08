1447a403422d4d706d48534040151d21a29cf5c9
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-02-08 13:55:34 +0530

    Create ktest.c

---
+++ task4/ktest.c
@@ -0,0 +1,456 @@
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+//#include "uarts.h"
+
+#include <math.h> //included to support power function
+#define ADC12 0b00100100
+#define ADC13 0b00100101
+#define RS 0
+#define RW 1
+#define EN 2
+#define lcd_port PORTC
+
+#define sbit(reg,bit)	reg |= (1<<bit)			// Macro defined for Setting a bit of any register.
+#define cbit(reg,bit)	reg &= ~(1<<bit)		// Macro defined for Clearing a bit of any register.
+void bit0(uint8_t val);
+void bit0(uint8_t val)
+{
+	int8_t ptr;
+	for(ptr=7;ptr>=0;ptr--)
+	{
+		if ((val & (1<<ptr))==0)
+		{
+			lcd_wr_char('0');
+		}
+		else
+		{
+			lcd_wr_char('1');
+		}
+	}
+}
+void init_ports();
+void lcd_reset();
+void lcd_init();
+void lcd_wr_command(unsigned char);
+void lcd_wr_char(char);
+void lcd_line1();
+void lcd_line2();
+void lcd_string(char*);
+
+unsigned int temp;
+unsigned int unit;
+unsigned int tens;
+unsigned int hundred;
+unsigned int thousand;
+unsigned int million;
+
+
+void port_init();
+void timer5_init();
+void velocity(unsigned char, unsigned char);
+void motors_delay();
+
+unsigned char ADC_Conversion(unsigned char);
+unsigned char ADC_Value;
+unsigned char flag = 0;
+unsigned char left_sensor = 0;
+unsigned char center_sensor = 0;
+unsigned char right_sensor = 0;
+unsigned char lsharp=0;
+unsigned char rsharp=0;
+unsigned char lvalue=0;
+unsigned char rvalue=0;
+
+
+//Function to configure LCD port
+void lcd_port_config (void)
+{
+ DDRC = DDRC | 0xF7; //all the LCD pin's direction set as output
+ PORTC = PORTC & 0x80; // all the LCD pins are set to logic 0 except PORTC 7
+}
+void lcd_set_4bit()
+{
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x20;				//Sending 2 to initialise LCD 4-bit mode
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(1);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	
+}
+
+//Function to Initialize LCD
+void lcd_init()
+{
+	_delay_ms(1);
+
+	lcd_wr_command(0x28);			//LCD 4-bit mode and 2 lines.
+	lcd_wr_command(0x01);
+	lcd_wr_command(0x06);
+	lcd_wr_command(0x0E);
+	lcd_wr_command(0x80);
+	
+}
+
+//Function to Write Command on LCD
+void lcd_wr_command(unsigned char cmd)
+{
+	unsigned char temp;
+	temp = cmd;
+	temp = temp & 0xF0;
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+	
+	cmd = cmd & 0x0F;
+	cmd = cmd<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= cmd;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+//Function to Write Data on LCD
+void lcd_wr_char(char letter)
+{
+	char temp;
+	temp = letter;
+	temp = (temp & 0xF0);
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+
+	letter = letter & 0x0F;
+	letter = letter<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= letter;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+//Function to bring cursor at home position
+void lcd_home()
+{
+	lcd_wr_command(0x80);
+}
+
+//Function to Print String on LCD
+void lcd_string(char *str)
+{
+	while(*str != '\0')
+	{
+		lcd_wr_char(*str);
+		str++;
+	}
+}
+
+//Position the LCD cursor at "row", "column".
+
+void lcd_cursor (char row, char column)
+{
+	switch (row) {
+		case 1: lcd_wr_command (0x80 + column - 1); break;
+		case 2: lcd_wr_command (0xc0 + column - 1); break;
+		case 3: lcd_wr_command (0x94 + column - 1); break;
+		case 4: lcd_wr_command (0xd4 + column - 1); break;
+		default: break;
+	}
+}
+
+//Function To Print Any input value up to the desired digit on LCD
+void lcd_print (char row, char coloumn, unsigned int value, int digits)
+{
+	unsigned char flag=0;
+	if(row==0||coloumn==0)
+	{
+		lcd_home();
+	}
+	else
+	{
+		lcd_cursor(row,coloumn);
+	}
+	if(digits==5 || flag==1)
+	{
+		million=value/10000+48;
+		lcd_wr_char(million);
+		flag=1;
+	}
+	if(digits==4 || flag==1)
+	{
+		temp = value/1000;
+		thousand = temp%10 + 48;
+		lcd_wr_char(thousand);
+		flag=1;
+	}
+	if(digits==3 || flag==1)
+	{
+		temp = value/100;
+		hundred = temp%10 + 48;
+		lcd_wr_char(hundred);
+		flag=1;
+	}
+	if(digits==2 || flag==1)
+	{
+		temp = value/10;
+		tens = temp%10 + 48;
+		lcd_wr_char(tens);
+		flag=1;
+	}
+	if(digits==1 || flag==1)
+	{
+		unit = value%10 + 48;
+		lcd_wr_char(unit);
+	}
+	if(digits>5)
+	{
+		lcd_wr_char('E');
+	}
+	
+}
+
+//ADC pin configuration
+void adc_pin_config (void)
+{
+ DDRF = 0x00; 
+ PORTF = 0x00;
+ DDRK = 0x00;
+ PORTK = 0x00;
+}
+
+//Function to configure ports to enable robot's motion
+void motion_pin_config (void) 
+{
+ DDRA = DDRA | 0x0F;
+ PORTA = PORTA & 0xF0;
+ DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+ PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
+}
+
+//Function to Initialize PORTS
+void port_init()
+{
+	lcd_port_config();
+	adc_pin_config();
+	motion_pin_config();	
+}
+
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+void ADC_setup(void)
+{
+	ADMUX = (1<<REFS0) | (1<<REFS1)|(0<<ADLAR); // select external AREF
+	//DIDR2 = (1<<ADC8D) | (1<<ADC9D); // digital input disable
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); // enable ADC, 128 clk divider
+}
+void ADC_select(uint8_t chan)
+{
+	ADMUX = (ADMUX & 0xE0) | (chan);   //select channel (MUX0-4 bits)
+	ADCSRB = (ADCSRB & 0xDF) | (chan);   //select channel (MUX5 bit)
+	ADCSRA|=(1<<ADSC);
+	
+}
+void ADC_read(void)
+{
+	bit0(ADCW);
+	
+	_delay_ms(8000);
+}
+
+void adc_init()
+{
+	ADCSRA = 0x00;
+	ADCSRB = 0x00;		//MUX5 = 0
+	ADMUX = 0x20;		//Vref=5V external --- ADLAR=1 --- MUX4:0 = 0000
+	ACSR = 0x80;
+	ADCSRA = 0x86;		//ADEN=1 --- ADIE=1 --- ADPS2:0 = 1 1 0
+}
+
+//Function For ADC Conversion
+unsigned char ADC_Conversion(unsigned char Ch) 
+{
+	unsigned char a;
+	if(Ch>7)
+	{
+		ADCSRB = 0x08;
+	}
+	Ch = Ch & 0x07;  			
+	ADMUX= 0x20| Ch;	   		
+	ADCSRA = ADCSRA | 0x40;		//Set start conversion bit
+	while((ADCSRA&0x10)==0);	//Wait for conversion to complete
+	a=ADCH;
+	ADCSRA = ADCSRA|0x10; //clear ADIF (ADC Interrupt Flag) by writing 1 to it
+	ADCSRB = 0x00;
+	return a;
+	//bit0(ADC);
+}
+unsigned int Sharp_conversion(unsigned char adc_reading)
+{
+	float distance;
+	unsigned int distanceInt;
+	distance = (int)(10.00*(2799.6*(1.00/(pow(adc_reading,1.1546)))));
+	distanceInt = (int)distance;
+	if(distanceInt>800)
+	{
+		distanceInt=800;
+	}
+	return distanceInt;
+	
+}
+
+//Function To Print Sesor Values At Desired Row And Coloumn Location on LCD
+void print_sensor(char row, char coloumn,unsigned char channel)
+{
+	
+	ADC_Value = ADC_Conversion(channel);
+	lcd_print(row, coloumn, ADC_Value, 3);
+}
+
+//Function for velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+ unsigned char PortARestore = 0;
+
+ Direction &= 0x0F; 		// removing upper nibbel for the protection
+ PortARestore = PORTA; 		// reading the PORTA original status
+ PortARestore &= 0xF0; 		// making lower direction nibbel to 0
+ PortARestore |= Direction; // adding lower nibbel for forward command and restoring the PORTA status
+ PORTA = PortARestore; 		// executing the command
+}
+
+void forward (void) 
+{
+  motion_set (0x05);
+}
+
+void stop (void)
+{
+  motion_set (0x00);
+}
+void buzzer()
+{
+	DDRB=0x01;
+	PORTB=0x01;
+	_delay_ms(5000);
+	PORTB=0x00;
+}
+
+void init_devices (void)
+{
+ 	cli(); //Clears the global interrupts
+	port_init();
+	adc_init();
+	timer5_init();
+	sei();   //Enables the global interrupts
+}
+
+//Main Function
+int main()
+{
+	init_devices();
+	lcd_set_4bit();
+	lcd_init();
+	ADC_setup();
+	
+	while(1)
+	{
+        lcd_cursor(1,3);
+        unsigned char left;
+		left_sensor = ADC_Conversion(3);
+		//bit0(left_sensor);
+			//Getting data of Left WL Sensor
+		center_sensor = ADC_Conversion(2);	//Getting data of Center WL Sensor
+		right_sensor = ADC_Conversion(1);	//Getting data of Right WL Sensor
+		//ADC_select(ADC12);
+		//lvalue=ADCW;
+		//ADC_select(ADC13);
+		//rvalue=ADCW;
+		//_delay_ms(8000);
+		
+		
+		lvalue=ADC_Conversion(4);
+		rvalue=ADC_Conversion(5);
+		//bit0(lvalue);
+		//lcd_wr_char("/n");
+	left=Sharp_conversion(lvalue);
+	//bit0(lvalue);
+		lcd_print(1,1,left,5);
+
+		
+        
+		//print_sensor(1,1,4);	
+		//print_sensor(1,5,5);	
+			
+		
+	
+
+
+	}
+}
