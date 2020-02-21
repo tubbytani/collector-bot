15845fcd748c9ad9af10791e38523a0c2913cfb0
SravaniMalekar <60072148+SravaniMalekar@users.noreply.github.com>
2020-02-21 13:06:41 +0530

    Create placehighfinal.c

---
+++ arm/placehighfinal.c
@@ -0,0 +1,349 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 18-02-2020 21:40:44
+ *  Author: Sravani Malekar
+ */ 
+
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <math.h> //included to support power function
+
+
+#define RS 0
+#define RW 1
+#define EN 2
+#define lcd_port PORTC
+
+#define sbit(reg,bit)	reg |= (1<<bit)			// Macro defined for Setting a bit of any register.
+#define cbit(reg,bit)	reg &= ~(1<<bit)		// Macro defined for Clearing a bit of any register.
+
+
+void lcd_reset();
+void lcd_init();
+void lcd_wr_command(unsigned char);
+void lcd_wr_char(char);
+void lcd_line1();
+void lcd_line2();
+void lcd_string(char*);
+
+unsigned int temp;
+unsigned int unit;
+unsigned int tens;
+unsigned int hundred;
+unsigned int thousand;
+unsigned int million;
+void port_init();
+void timer5_init();
+void velocity(unsigned char, unsigned char);
+void motors_delay();
+void white_line(void);
+void fwd(void);
+void bam(void);
+void zoom(void);
+void fwd1(void);
+void servo_2(unsigned char);
+void servo_3(unsigned char);
+void servo_1(unsigned char );
+void servo_1_free (void);void servo_2_free (void);void servo_3_free (void);
+void init_devices (void);
+
+unsigned char ADC_Conversion(unsigned char);
+unsigned char ADC_Value;
+unsigned int value;
+unsigned char sharp=0, distance=0, adc_reading=0;
+unsigned char flag = 0;
+int counter=2;
+int final_array[]={1,2,3,4,0,13,13,13,7,6,0,15};
+int new_array[]=  {1,2,3,4,8,13,0 ,0 ,6,5,0,0 };
+int house_array[]={1,2,3,4,5,13,13,13,6,15,15,15};
+int cm_array[]={1,2,3,4};
+int i=3;
+int j=0;
+int l=0;
+unsigned char left_sensor = 0;
+unsigned char center_sensor = 0;
+unsigned char right_sensor = 0;
+unsigned char lvalue=0,rvalue=0;
+void adc_pin_config (void)
+{
+	DDRF = 0x00;
+	PORTF = 0x00;
+	DDRK = 0x00;
+	PORTK = 0x00;
+}
+
+//Function to configure ports to enable robot's motion
+void motion_pin_config (void)
+{
+	DDRA = DDRA | 0x0F;
+	PORTA = PORTA & 0xF0;
+	DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+	PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
+}
+
+//Configure PORTB 5 pin for servo motor 1 operation
+void servo1_pin_config (void)
+{
+ DDRB  = DDRB | 0x20;  //making PORTB 5 pin output
+ PORTB = PORTB | 0x20; //setting PORTB 5 pin to logic 1
+}
+
+//Configure PORTB 6 pin for servo motor 2 operation
+void servo2_pin_config (void)
+{
+ DDRB  = DDRB | 0x40;  //making PORTB 6 pin output
+ PORTB = PORTB | 0x40; //setting PORTB 6 pin to logic 1
+}
+
+//Configure PORTB 7 pin for servo motor 3 operation
+void servo3_pin_config (void)
+{
+ DDRB  = DDRB | 0x80;  //making PORTB 7 pin output
+ PORTB = PORTB | 0x80; //setting PORTB 7 pin to logic 1
+}
+
+//Initialize the ports
+void port_init(void)
+{
+	adc_pin_config();
+	motion_pin_config();
+ servo1_pin_config(); //Configure PORTB 5 pin for servo motor 1 operation
+ servo2_pin_config(); //Configure PORTB 6 pin for servo motor 2 operation 
+ servo3_pin_config(); //Configure PORTB 7 pin for servo motor 3 operation  
+}
+
+//TIMER1 initialization in 10 bit fast PWM mode  
+//prescale:256
+// WGM: 7) PWM 10bit fast, TOP=0x03FF
+// actual value: 52.25Hz 
+void timer1_init(void)
+{
+ TCCR1B = 0x00; //stop
+ TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
+ TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
+ OCR1AH = 0x03;	//Output compare Register high value for servo 1
+ OCR1AL = 0xFF;	//Output Compare Register low Value For servo 1
+ OCR1BH = 0x03;	//Output compare Register high value for servo 2
+ OCR1BL = 0xFF;	//Output Compare Register low Value For servo 2
+ OCR1CH = 0x03;	//Output compare Register high value for servo 3
+ OCR1CL = 0xFF;	//Output Compare Register low Value For servo 3
+ ICR1H  = 0x03;	
+ ICR1L  = 0xFF;
+ TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
+ 					For Overriding normal port functionality to OCRnA outputs.
+				  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
+ TCCR1C = 0x00;
+ TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+}
+
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+
+void adc_init()
+{
+	ADCSRA = 0x00;
+	ADCSRB = 0x00;		//MUX5 = 0
+	ADMUX = 0x20;		//Vref=5V external --- ADLAR=1 --- MUX4:0 = 0000
+	ACSR = 0x80;
+	ADCSRA = 0x86;		//ADEN=1 --- ADIE=1 --- ADPS2:0 = 1 1 0
+}
+
+//Function For ADC Conversion
+unsigned char ADC_Conversion(unsigned char Ch) 
+{
+	unsigned char a;
+	if(Ch>7)
+	{
+		ADCSRB = 0x08;
+	}
+	Ch = Ch & 0x07;  			
+	ADMUX= 0x20| Ch;	   		
+	ADCSRA = ADCSRA | 0x40;		//Set start conversion bit
+	while((ADCSRA&0x10)==0);	//Wait for conversion to complete
+	a=ADCH;
+	ADCSRA = ADCSRA|0x10; //clear ADIF (ADC Interrupt Flag) by writing 1 to it
+	ADCSRB = 0x00;
+	return a;
+}
+//Function for velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+	unsigned char PortARestore = 0;
+	Direction &= 0x0F; 		// removing upper nibble for the protection
+	PortARestore = PORTA; 		// reading the PORTA original status
+	PortARestore &= 0xF0; 		// making lower direction nibble to 0
+	PortARestore |= Direction; // adding lower nibble for forward command and restoring the PORTA status
+	PORTA = PortARestore; 		// executing the command
+}
+
+void forward (void)
+{
+	motion_set (0x05);
+}
+
+void stop (void)
+{
+	motion_set (0x00);
+}
+void back (void) //both wheels backward
+{
+	motion_set(0x0A);
+}
+void left (void) //Left wheel backward, Right wheel forward
+{
+	motion_set(0x09);
+}
+void right (void) //Left wheel forward, Right wheel backward
+{
+	motion_set(0x06);
+}
+void soft_left (void) //Left wheel stationary, Right wheel forward
+{
+	motion_set(0x04);
+}
+void soft_right (void) //Left wheel forward, Right wheel is stationary
+{
+	motion_set(0x02);
+}
+
+void soft_left_2 (void) //Left wheel backward, right wheel stationary
+{
+	motion_set(0x01);
+}
+
+void soft_right_2 (void) //Left wheel stationary, Right wheel backward
+{
+	motion_set(0x08);
+}
+
+void uturn(void)
+{
+	left();
+	_delay_ms(600);
+	stop();
+}
+void buzzer()
+{
+	DDRB=0x01;
+	PORTB=0x01;
+	_delay_ms(500);
+	PORTB=0x00;
+}
+//Function to initialize all the peripherals
+void init_devices(void)
+{
+ cli(); //disable all interrupts
+ port_init();
+ timer1_init();
+ sei(); //re-enable interrupts 
+}
+
+
+//Function to rotate Servo 1 by a specified angle in the multiples of 1.86 degrees
+void servo_1(unsigned char degrees)  
+{
+ float PositionPanServo = 0;
+  PositionPanServo = ((float)degrees / 1.86) + 35.0;
+ OCR1AH = 0x00;
+ OCR1AL = (unsigned char) PositionPanServo;
+}
+
+
+//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+void servo_2(unsigned char degrees)
+{
+ float PositionTiltServo = 0;
+ PositionTiltServo = ((float)degrees / 1.86) + 35.0;
+ OCR1BH = 0x00;
+ OCR1BL = (unsigned char) PositionTiltServo;
+}
+
+//Function to rotate Servo 3 by a specified angle in the multiples of 1.86 degrees
+void servo_3(unsigned char degrees)
+{
+ float PositionServo = 0;
+ PositionServo = ((float)degrees / 1.86) + 35.0;
+ OCR1CH = 0x00;
+ OCR1CL = (unsigned char) PositionServo;
+}
+
+//servo_free functions unlocks the servo motors from the any angle 
+//and make them free by giving 100% duty cycle at the PWM. This function can be used to 
+//reduce the power consumption of the motor if it is holding load against the gravity.
+
+void servo_1_free (void) //makes servo 1 free rotating
+{
+ OCR1AH = 0x03; 
+ OCR1AL = 0xFF; //Servo 1 off
+}
+
+void servo_2_free (void) //makes servo 2 free rotating
+{
+ OCR1BH = 0x03;
+ OCR1BL = 0xFF; //Servo 2 off
+}
+
+void servo_3_free (void) //makes servo 3 free rotating
+{
+ OCR1CH = 0x03;
+ OCR1CL = 0xFF; //Servo 3 off
+} 
+
+//Main function
+void main(void)
+{
+unsigned char p= 100,q=110,r=0;
+
+init_devices();
+
+for(q=110;q<160;q++)
+{
+	servo_2(q);
+	_delay_ms(50);
+}
+for(p=100;p>50;p--)
+{
+	servo_1(p);
+	_delay_ms(50);
+}
+for(r=0;r<50;r++)
+{
+	servo_3(r);
+	_delay_ms(50);
+}
+for(p=50;p<100;p++)
+{
+	servo_1(p);
+	_delay_ms(50);
+}
+ _delay_ms(2000);
+ servo_1_free(); 
+ servo_2_free();
+ servo_3_free();
+ while(1);
+}
